c04557ad92851ae573d8a13894e42b59b2e6a158
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-27 20:48:43 +0530

    array

--- Array1.java
+++ Array1.java
@@ -1,6 +1,23 @@
 public class Array1 {
     public static void main(String args[])
     {
+        // 1 
+        int Arr1[] = {11,21,51,101};
+
+        //2
+        int Arr2[] = new int[4];
+        Arr2[0] = 11;
+        Arr2[1] = 21;
+        Arr2[2] = 51;
+        Arr2[3] = 101;
+
+        System.out.println("Length of Array is :"+Arr1.length);
         
+        int iCnt = 0;
+        //   1          2                 3
+        for(iCnt = 0;iCnt<Arr1.length;iCnt++)
+        {
+            System.out.println(Arr1[iCnt]); // 4
+        }
     }
 }
---
+++ Array2.java
@@ -0,0 +1,29 @@
+public class Array2 {
+    public static void main(String args[])
+    {
+        int Arr[][] = {{10,20,30},{40,50,60},{70,80,90}};
+
+        int Brr[][] = new int[3][3];
+
+        Brr[0][0] = 10;
+        Brr[0][1] = 20;
+        Brr[0][2] = 30;
+
+        Brr[1][0] = 40;
+        Brr[1][1] = 50;
+        Brr[1][2] = 60;
+
+        Brr[2][0] = 70;
+        Brr[2][1] = 80;
+        Brr[2][2] = 90;
+
+        for(int i = 0 ;i<Arr.length;i++)
+        {
+            for(int j = 0;j<Arr[i].length;j++)
+            {
+                System.out.print(Arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
