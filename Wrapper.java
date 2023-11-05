ce51b5f2fd7967670cdc62996d72248af723eeb5
nitinbhopale <nitinbhopale53@gmail.com>
2023-11-05 10:42:14 +0530

    commit

---
+++ Wrapper.java
@@ -0,0 +1,29 @@
+public class Wrapper {
+    public static void main(String arg[])
+    {
+        int no = 11;
+        Integer iboj = no; // Boxing
+
+
+
+        System.out.println(no);
+        System.out.println(iboj);
+
+        int i = iboj; // Unboxing
+
+        
+    }
+}
+
+/*  
+    Datatype    Wrapper class
+
+    boolean     Boolean
+    byte        Byte
+    char        Character
+    short       Short
+    int         Integer
+    float       Float
+    double      Double
+    long        Long
+*/
