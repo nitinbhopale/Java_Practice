101e53efa9ece0cb6c3ca1a3b2cadb7f0c8c4cb4
nitinbhopale <nitinbhopale53@gmail.com>
2023-11-05 10:56:31 +0530

    commit

--- CreateFile.java
+++ CreateFile.java
@@ -1,3 +1,19 @@
+import java.io.*;
+
 public class CreateFile {
-    
+    public static void main(String arg[])
+    {
+        try
+        {
+            File fobj = new File("PPA.txt");
+
+            fobj.createNewFile();
+        }
+        catch(Exception obj)
+        {
+            
+        }
+
+        
+    }
 }
